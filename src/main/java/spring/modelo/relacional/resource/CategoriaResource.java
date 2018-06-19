c4144a9c0bfd31cde16a76b93f3efef1a64d0fb8
Guilherme Macedo <guilhermericardord@gmail.com>
2018-06-19 17:31:13 -0300

    listando todas as categorias com dto

--- src/main/java/spring/modelo/relacional/resource/CategoriaResource.java
+++ src/main/java/spring/modelo/relacional/resource/CategoriaResource.java
@@ -1,6 +1,8 @@
 package spring.modelo.relacional.resource;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import spring.modelo.relacional.domain.Categoria;
+import spring.modelo.relacional.dto.CategoriaDTO;
 import spring.modelo.relacional.services.CategoriaServico;
 
 @RestController
@@ -46,5 +49,13 @@ public class CategoriaResource {		//Rest
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); //convertendo lista para listadto
+		return ResponseEntity.ok().body(listDTO);
+		
 	}
 }
--- src/main/java/spring/modelo/relacional/services/CategoriaServico.java
+++ src/main/java/spring/modelo/relacional/services/CategoriaServico.java
@@ -1,5 +1,6 @@
 package spring.modelo.relacional.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,34 +11,38 @@ import spring.modelo.relacional.domain.Categoria;
 import spring.modelo.relacional.repositories.CategoriaRepository;
 
 @Service
-public class CategoriaServico {		//Chamada de serviço
-	
+public class CategoriaServico { // Chamada de serviço
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado id :) "+ id + " Tipo: "+ Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado id :) " + id + " Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		findById(obj.getId());
-		return repo.save(obj);	//serve tanto para inserir qto para atualizar
+		return repo.save(obj); // serve tanto para inserir qto para atualizar
 	}
 
 	public void delete(Integer id) {
 		findById(id);
 		try {
-			repo.deleteById(id);	
-		}catch(DataIntegrityViolationException e){		//Delete em cascata	
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) { // Delete em cascata
 			throw new DataIntegrityException("Não é possivel excluir categoria que possui a produtos");
 		}
-		
-		
+
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
