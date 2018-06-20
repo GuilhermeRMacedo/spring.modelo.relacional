94a35c7f80db7d3334de51777c474877295eaf00
Guilherme Macedo <guilhermericardord@gmail.com>
2018-06-20 17:01:42 -0300

    validação com bean validation

--- src/main/java/spring/modelo/relacional/services/CategoriaServico.java
+++ src/main/java/spring/modelo/relacional/services/CategoriaServico.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import spring.modelo.relacional.domain.Categoria;
+import spring.modelo.relacional.dto.CategoriaDTO;
 import spring.modelo.relacional.repositories.CategoriaRepository;
 
 @Service
@@ -44,5 +48,14 @@ public class CategoriaServico { // Chamada de serviço
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
